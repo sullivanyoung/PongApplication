ca404cc4718f5dd219248fcc06b25b62c87e7b75
Sullivan Young <sullivangyoung@gmail.com>
2020-12-09 08:48:38 -0500

    Game functionality

--- Pong/Pong.cs
+++ Pong/Pong.cs
@@ -53,7 +53,90 @@ namespace Pong
 
         private void timerTick(object sender, EventArgs e)
         {
+            // this is the main timer event and will trigger every 20 milliseconds
 
+            playerScore.Text = "" + userScore;
+            cpuLabel.Text = "" + cpuScore;
+
+            ball.Top -= bally; // assigns the ball TOP to all Y integer
+            ball.Left -= ballx; // assigns the ball LEFT to ball X integer
+
+            cpu.Top += speed; //assignment the CPU top speed integer
+
+            if (cpuScore < 5)
+            {
+                //if the cpu has reached the top or bottom of the screen
+                if (cpu.Top < 0 || cpu.Top > 455)
+                {
+                    //changes the speed direction to move it back up/down
+                    speed = -speed;
+                }
+            }
+            else
+            {
+                //if score isgreater than 5 make the game more difficult by allowing CPU to follow ball
+                cpu.Top = ball.Top + 30;
+            }
+
+            //create cpu point and reset
+
+            if (ball.Left < 0)
+            {
+                ball.Left = 434; //resets ball
+                ballx = -ballx; //changes ball direction
+                ballx -= 2; //increases speed
+                cpuScore++;
+            }
+
+            //create user point and reset
+
+            if(ball.Left + ball.Width > ClientSize.Width)
+            {
+                ball.Left = 434; //resets ball
+                ballx = -ballx; //changes ball direction
+                ballx += 2; //increases speed
+                userScore++;
+            }
+
+            //ball movement
+            //if ball reaches top or bottom of screen
+            if(ball.Top < 0 || ball.Top + ball.Height > ClientSize.Height)
+            {
+                //reverse speed of ball to stay on screen
+                bally = -bally;
+            }
+
+            if(ball.Bounds.IntersectsWith(player.Bounds) || ball.Bounds.IntersectsWith(cpu.Bounds))
+            {
+                ballx = -ballx;
+            }
+
+            //player movement
+            //checking if boolean is true and heigh of boundary has not been reached
+            if(goUp == true && player.Top > 0)
+            {
+                player.Top -= 8;
+            }
+            if(goDown == true && player.Top < 455)
+            {
+                player.Top += 8;
+            }
+
+            //final score / ending the game if the player scores more than 10 timer will stop and message box shown
+            if(userScore > 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("You Win!");
+                Application.Exit();
+            }
+            if(cpuScore > 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("CPU Wins!");
+                Application.Exit();
+            }
         }
+
+
     }
 }
